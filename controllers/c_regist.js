d08b6ddd98299ef9c6c16e2ef07fa16432830e1c
Laminhdatau <Laminhdataulamin2@gmail.com>
2023-09-06 09:53:30 +0800

    gas

--- controllers/c_regist.js
+++ controllers/c_regist.js
@@ -1,18 +1,39 @@
-
-const m_regist = require('../models/m_regist'); 
+const formidable = require('formidable');
+const fs = require('fs');
+const path = require('path');
+const m_regist = require('../models/m_regist');
 
 const register = (req, res) => {
-  const { username, password, id_role, nik } = req.body;
+  const form = new formidable.IncomingForm();
 
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(500).json({ error: 'Gagal mengunggah gambar' });
+    }
 
-    m_regist.register(username, password, id_role, nik, (err, result) => {
+    const { username, password, id_role, nik } = fields;
+    const user_image = files.user_image;
+
+    if (!user_image) {
+      return res.status(400).json({ error: 'Gambar tidak ditemukan' });
+    }
+
+    const imagePath = path.join(__dirname, '../uploads', Date.now() + '-' + user_image.name);
+
+    fs.rename(user_image.path, imagePath, (err) => {
       if (err) {
-        res.status(500).json({ error: 'Gagal melakukan registrasi' });
-      } else {
-        res.status(200).json({ message: 'Registrasi berhasil' });
+        return res.status(500).json({ error: 'Gagal menyimpan gambar' });
       }
-    });
- };
 
+      m_regist.register(username, password, id_role, nik, imagePath, (err, result) => {
+        if (err) {
+          res.status(500).json({ error: 'Gagal melakukan registrasi' });
+        } else {
+          res.status(200).json({ message: 'Registrasi berhasil' });
+        }
+      });
+    });
+  });
+};
 
 module.exports = { register };
--- models/m_regist.js
+++ models/m_regist.js
@@ -2,10 +2,10 @@ const db = require('../koneksi/config');
 const uuid = require('uuid');
 
 const m_regist = {
-  register: (username, password, id_role, nik, callback) => {
+  register: (username, password, id_role, nik, user_image, callback) => {
     const id_user = uuid.v4();
-    const sql = 'INSERT INTO t_user (id_user, username, password, id_role, nik) VALUES (?, ?, ?, ?, ?)';
-    const values = [id_user, username, password, id_role, nik];
+    const sql = 'INSERT INTO t_user (id_user, username, password, id_role, nik, user_image) VALUES (?, ?, ?, ?, ?, ?)';
+    const values = [id_user, username, password, id_role, nik, user_image];
 
     db.query(sql, values, (err, result) => {
       if (err) {
