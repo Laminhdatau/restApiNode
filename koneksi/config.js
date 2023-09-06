db5d104fa14e3c43e233fa4418f2392a87f2b704
Laminhdatau <Laminhdataulamin2@gmail.com>
2023-09-06 23:12:46 +0800

    kon

--- koneksi/config.js
+++ koneksi/config.js
@@ -1,11 +1,20 @@
 const mysql = require('mysql');
 
-// Konfigurasi koneksi MySQL
+// Konfigurasi koneksi MySQL lokal
+// const db = mysql.createConnection({
+//   host: '192.168.123.21',
+//   user: 'root',
+//   password: 'root',
+//   database: 'absensi'
+// });
+
+
+// Hosting
 const db = mysql.createConnection({
-  host: '192.168.123.21',
-  user: 'root',
-  password: 'root',
-  database: 'absensi'
+  host: '172.18.0.1',
+  user: 'id19981001_absensi',
+  password: '@Absensi123',
+  database: 'id19981001_absensi'
 });
 
 
